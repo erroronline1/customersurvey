9469f98ffe74d05fb27d14c4a60028ebf0edb787
error on line 1 <dev@erroronline.one>
2020-11-06 22:31:41 +0100

    variable sql-table settings for more customizabilty

--- ressources/api.php
+++ ressources/api.php
@@ -9,36 +9,36 @@ include('dbconnect.php');
 $payload = json_decode(file_get_contents("php://input"));
 // assign values to (legacy) db-columns
 $legacy_translate=[
-    'Meinung' => $payload->general,
-    'Hilfsmittel' => urldecode($payload->text3),
-    'Vorschlag' => urldecode($payload->text2),
-    'Lob' => urldecode($payload->text1),
-    'Erreichbarkeit' => $payload->query1,
-    'Bearbeitungszeit' => $payload->query2,
-    'Freundlichkeit' => $payload->query3,
-    'Kompetenz' => $payload->query4
+	$db[0]['fields'][2] => $payload->general,
+	$db[0]['fields'][3] => urldecode($payload->text3),
+	$db[0]['fields'][4] => urldecode($payload->text2),
+	$db[0]['fields'][5] => urldecode($payload->text1),
+	$db[0]['fields'][6] => $payload->query1,
+	$db[0]['fields'][7] => $payload->query2,
+	$db[0]['fields'][8] => $payload->query3,
+	$db[0]['fields'][9] => $payload->query4
 ];
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-    
-    // prepare and execute sql query
-    foreach($legacy_translate as $key=>$value){
-        $insertion .= ', "' . $value . '"';
-    }
-    $mysqli->query('INSERT INTO kza VALUES (NULL, NOW(), ' . substr($insertion, 2) . ')');
-    
-    //return current row-id
-    echo $mysqli->insert_id ? : null;
+	
+	// prepare and execute sql query
+	foreach($legacy_translate as $key=>$value){
+		$insertion .= ', "' . $value . '"';
+	}
+	$mysqli->query('INSERT INTO ' . $db[0]['table'] . ' VALUES (NULL, NOW(), ' . substr($insertion, 2) . ')');
+	
+	//return current row-id
+	echo $mysqli->insert_id ? : null;
 }
 elseif ($_SERVER['REQUEST_METHOD'] == 'PUT'){
-    // prepare and execute sql query
-    foreach($legacy_translate as $key=>$value){
-        $update .= ', ' . $key . '="' . $value . '"';
-    }
-    $mysqli->query('UPDATE kza SET ' . substr($update, 2) . ' WHERE id=' . $payload->id);
-    
-    //return current row-id
-    echo $payload->id ? : null;
+	// prepare and execute sql query
+	foreach($legacy_translate as $key=>$value){
+		$update .= ', ' . $key . '="' . $value . '"';
+	}
+	$mysqli->query('UPDATE ' . $db[0]['table'] . ' SET ' . substr($update, 2) . ' WHERE id=' . $payload->id);
+	
+	//return current row-id
+	echo $payload->id ? : null;
 
 }
 elseif ($_SERVER['REQUEST_METHOD'] == 'GET'){
