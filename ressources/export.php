87b2352c750eb993937cf689f511e633cd722935
error on line 1 <dev@erroronline.one>
2020-11-07 00:24:45 +0100

    declared constant DB

--- ressources/export.php
+++ ressources/export.php
@@ -4,14 +4,14 @@ include('dbconnect.php');
 
 // mysql export
 if ($_GET['type'] == 'sql'){
-	$ressource = $mysqli->query("SHOW CREATE TABLE `" . $db[0]['table'] . "`");
+	$ressource = $mysqli->query("SHOW CREATE TABLE `" . DB['table'] . "`");
 	if ($ressource) {
-		$output .= "DROP TABLE `" . $db[0]['table'] . "`;\r\n"; 
+		$output .= "DROP TABLE `" . DB['table'] . "`;\r\n"; 
 		$create = $ressource->fetch_array(MYSQLI_ASSOC);
 		$create['Create Table'] .= ";";
 		$output .= str_replace("\n", "", $create['Create Table']) . "\r\n";
 	}
-	$q_save = $mysqli->query("SELECT * FROM ".$db[0]['table']);
+	$q_save = $mysqli->query("SELECT * FROM ".DB['table']);
 	$fieldnames = 0;
 	$fields = '';
 	$values = '';
@@ -26,13 +26,13 @@ if ($_GET['type'] == 'sql'){
 		$values .= ",(" . substr($temp_values, 1) . ")\n";
 		$fieldnames++;
 	}
-	if ($values) $output .= "INSERT INTO " . $db[0]['table'] . "\n(" . substr($fields, 1) . ")\nVALUES\n" . substr($values, 1) . ";\r\n";
+	if ($values) $output .= "INSERT INTO " . DB['table'] . "\n(" . substr($fields, 1) . ")\nVALUES\n" . substr($values, 1) . ";\r\n";
 	$filename = 'customer_survey_database_export_';
 }
 
 // csv export
 elseif ($_GET['type'] == 'csv'){
-	$t_save = $mysqli->query("SELECT * FROM " . $db[0]['table'] . " ORDER BY " . $db[0]['fields'][0]);
+	$t_save = $mysqli->query("SELECT * FROM " . DB['table'] . " ORDER BY " . DB['fields'][0]);
 	$fieldnames = 0;
 	$fields = "";
 	$values = "";
@@ -56,11 +56,11 @@ elseif ($_GET['type'] == 'rtf'){
 
 	// total and main review
 	$total = $mysqli->query("SELECT
-		MIN(" . $db[0]['fields'][1] . ") AS 'from',
-		MAX(" . $db[0]['fields'][1] . ") AS 'to',
-		COUNT(" . $db[0]['fields'][0] . ") AS 'count',
-		AVG(Meinung) AS 'avg' FROM " . $db[0]['table'] . "
-		WHERE " . $db[0]['fields'][0] . ">1")->fetch_assoc();
+		MIN(" . DB['fields'][1] . ") AS 'from',
+		MAX(" . DB['fields'][1] . ") AS 'to',
+		COUNT(" . DB['fields'][0] . ") AS 'count',
+		AVG(Meinung) AS 'avg' FROM " . DB['table'] . "
+		WHERE " . DB['fields'][0] . ">1")->fetch_assoc();
 	$output.="{\b Kundenzufriedenheitsanalyse basierend auf den Angaben von " . substr($total['from'], 0, 10) . " bis " . substr($total['to'], 0, 10) . "} \par {\b Gesamteinschätzung:}
 \line
 Von " . substr($total['from'], 0, 10) . " bis " . substr($total['to'], 0, 10) . " ergeben " . $total['count'] . " allgemeine Bewertungen eine Zufriedenheit von " . round($total['avg']*50, 2) . " %.
@@ -69,34 +69,34 @@ Von " . substr($total['from'], 0, 10) . " bis " . substr($total['to'], 0, 10) . 
 	// topic related statistics
 	for ($topic = 6; $topic < 10; $topic++){
 		$total=$mysqli->query("SELECT
-			MIN(" . $db[0]['fields'][1] . ") AS 'from',
-			MAX(" . $db[0]['fields'][1] . ") AS 'to',
-			COUNT(" . $db[0]['fields'][0] . ") AS 'count',
-			AVG(" . $db[0]['fields'][$topic] . ") AS 'avg'
-			FROM " . $db[0]['table'] . " WHERE " . $db[0]['fields'][$topic] . "!=''")->fetch_assoc();
+			MIN(" . DB['fields'][1] . ") AS 'from',
+			MAX(" . DB['fields'][1] . ") AS 'to',
+			COUNT(" . DB['fields'][0] . ") AS 'count',
+			AVG(" . DB['fields'][$topic] . ") AS 'avg'
+			FROM " . DB['table'] . " WHERE " . DB['fields'][$topic] . "!=''")->fetch_assoc();
 		if ($total['count'])
-			$output .= "In der Fragestellung {\i " . $db[0]['fields'][$topic] . "}
+			$output .= "In der Fragestellung {\i " . DB['fields'][$topic] . "}
 \line 
 Von " . substr($total['from'], 0, 10) . " bis " . substr($total['to'], 0, 10) . " ergeben " . $total['count'] . " abgegebene Bewertungen eine Zufriedenheit von " . round($total['avg']*50, 2) . " %.
 \par ";
-		else $output .= "{\i " . $db[0]['fields'][$topic] . "} wurde noch nicht beantwortet \line ";
+		else $output .= "{\i " . DB['fields'][$topic] . "} wurde noch nicht beantwortet \line ";
 	}
 	
 	// text inputs
 	$output .= "{\b Vorschläge und Lob}";
-	$q_comments = $mysqli->query("SELECT * FROM " . $db[0]['table'] . " WHERE " . $db[0]['fields'][4] . "!='' OR " . $db[0]['fields'][5] . "!=''");
+	$q_comments = $mysqli->query("SELECT * FROM " . DB['table'] . " WHERE " . DB['fields'][4] . "!='' OR " . DB['fields'][5] . "!=''");
 	while ($comments = $q_comments->fetch_assoc()){
 		$output .= "\par
 \line
-{\b ".substr($comments[$db[0]['fields'][1]],0,10)."} ".$comments[$db[0]['fields'][3]]."
+{\b ".substr($comments[DB['fields'][1]],0,10)."} ".$comments[DB['fields'][3]]."
 \line
 ";
 		foreach ($comments as $key => $value){
-			if ($key == $db[0]['fields'][0] || $key == $db[0]['fields'][1] || $key == $db[0]['fields'][2] || $key == $db[0]['fields'][3]) ;
-			elseif (($key == $db[0]['fields'][4] || $key == $db[0]['fields'][5]) && $value!='') $output .= $key . ": " . $value . "\line ";	
+			if ($key == DB['fields'][0] || $key == DB['fields'][1] || $key == DB['fields'][2] || $key == DB['fields'][3]) ;
+			elseif (($key == DB['fields'][4] || $key == DB['fields'][5]) && $value!='') $output .= $key . ": " . $value . "\line ";	
 			elseif ($value != '') $output .= " / " . $key . ": " . $value;	
 		}
-		$output .= " / Meinung: " . $comments[$db[0]['fields'][2]];
+		$output .= " / Meinung: " . $comments[DB['fields'][2]];
 	}
 	$output .= "
 \par
