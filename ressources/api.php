a8e14328852d9bca65c71ceadd2888f6d7f901b9
error on line 1 <dev@erroronline.one>
2020-11-07 00:24:16 +0100

    added database erasure, declared constant DB

--- ressources/api.php
+++ ressources/api.php
@@ -9,14 +9,14 @@ include('dbconnect.php');
 $payload = json_decode(file_get_contents("php://input"));
 // assign values to (legacy) db-columns
 $legacy_translate=[
-	$db[0]['fields'][2] => $payload->general,
-	$db[0]['fields'][3] => urldecode($payload->text3),
-	$db[0]['fields'][4] => urldecode($payload->text2),
-	$db[0]['fields'][5] => urldecode($payload->text1),
-	$db[0]['fields'][6] => $payload->query1,
-	$db[0]['fields'][7] => $payload->query2,
-	$db[0]['fields'][8] => $payload->query3,
-	$db[0]['fields'][9] => $payload->query4
+	DB['fields'][2] => $payload->general,
+	DB['fields'][3] => urldecode($payload->text3),
+	DB['fields'][4] => urldecode($payload->text2),
+	DB['fields'][5] => urldecode($payload->text1),
+	DB['fields'][6] => $payload->query1,
+	DB['fields'][7] => $payload->query2,
+	DB['fields'][8] => $payload->query3,
+	DB['fields'][9] => $payload->query4
 ];
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
@@ -25,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 	foreach($legacy_translate as $key=>$value){
 		$insertion .= ', "' . $value . '"';
 	}
-	$mysqli->query('INSERT INTO ' . $db[0]['table'] . ' VALUES (NULL, NOW(), ' . substr($insertion, 2) . ')');
+	$mysqli->query('INSERT INTO ' . DB['table'] . ' VALUES (NULL, NOW(), ' . substr($insertion, 2) . ')');
 	
 	//return current row-id
 	echo $mysqli->insert_id ? : null;
@@ -35,11 +35,18 @@ elseif ($_SERVER['REQUEST_METHOD'] == 'PUT'){
 	foreach($legacy_translate as $key=>$value){
 		$update .= ', ' . $key . '="' . $value . '"';
 	}
-	$mysqli->query('UPDATE ' . $db[0]['table'] . ' SET ' . substr($update, 2) . ' WHERE id=' . $payload->id);
+	$mysqli->query('UPDATE ' . DB['table'] . ' SET ' . substr($update, 2) . ' WHERE id=' . $payload->id);
 	
 	//return current row-id
 	echo $payload->id ? : null;
 
+}
+elseif ($_SERVER['REQUEST_METHOD'] == 'DELETE'){
+	if ($payload->confirm == CONFIRM_DELETION) {
+		$mysqli->query('TRUNCATE TABLE ' . DB['table']);
+		echo null;
+	}
+	else echo http_response_code(401);
 }
 elseif ($_SERVER['REQUEST_METHOD'] == 'GET'){
 }
